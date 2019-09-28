ef18b9b83756a16eecf49b8f990334ba030a82aa
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-09-28 09:22:59 -0600

    first commit

---
+++ BranchAndChicken/DataAccess/TrainerRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BranchAndChicken.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BranchAndChicken.DataAccess
+{
+    public class TrainerRepository
+    {
+
+        static List<Trainer> _trainers = new List<Trainer>
+        {
+            new Trainer
+            {
+                Name = "Nathan",
+                Specialty = Specialty.TaeCluckDoe,
+                YearsOfExperience = 0
+            },
+
+            new Trainer
+            {
+                Name = "Martin",
+                Specialty = Specialty.Chudo,
+                YearsOfExperience = 12
+            },
+
+            new Trainer
+            {
+                Name = "Adam",
+                Specialty = Specialty.ChravMcgaw,
+                YearsOfExperience = 3
+            }
+
+        };
+
+        internal ActionResult<Trainer> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Trainer> GetAll()
+        {
+            return _trainers;
+        }
+       
+        public Trainer Get(string name)
+        {
+            var trainer = _trainers.First(t => t.Name == name);
+            return trainer;
+        }
+
+        public void Remove (string name)
+        {
+            var trainer = Get(name);
+            _trainers.Remove(trainer);
+        }
+    }
+}
---
+++ BranchAndChicken/Models/Trainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BranchAndChicken.Models
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+        public int YearsOfExperience { get; set; }
+        public Specialty Specialty { get; set; }
+        public List<Chicken> Coop { get; set; }
+    }
+
+    public enum Specialty
+    {
+        Chudo,
+        Chousting,
+        TaeCluckDoe,
+        ChravMcgaw
+    }
+}
